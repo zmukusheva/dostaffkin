0268205e4a550fe2237c260468b134883af75e34
zmukusheva <zmukusheva@gmail.com>
2026-02-24 17:40:23 +0000

    build

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -4,6 +4,7 @@ import { Header } from '../../header/header';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { UpperCasePipe } from '@angular/common';
+import { DeliveryApi } from '../../services/delivery-api';
 
 declare var ymaps: any;
 
@@ -26,7 +27,7 @@ export class Order {
   public orderId: any = signal(null);
   public calculationResult: any = signal(null);
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private deliveryApi: DeliveryApi) {
     this.routeForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['', Validators.required],
@@ -149,7 +150,14 @@ export class Order {
       createdAt: new Date().toISOString(),
     };
 
-    console.log(payload);
-    this.orderId.set(1);
+    this.deliveryApi.createDelivery(payload).subscribe((response) => {
+      if ('error' in response) {
+        alert(response.error);
+        return;
+      }
+
+      this.orderId.set(response.id);
+    });
+    
   }
 }
